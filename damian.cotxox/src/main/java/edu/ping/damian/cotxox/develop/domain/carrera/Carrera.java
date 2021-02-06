6ef85e77b7692af3cfe3a81c7d41e7ce29978926
bellothornus2 <divanov@cifpfbmoll.eu>
2021-02-06 17:02:03 +0100

    feat(Carrera): :sparkles: we finished to add the setters to the Carrera Java file

--- damian.cotxox/src/main/java/edu/ping/damian/cotxox/develop/domain/carrera/Carrera.java
+++ damian.cotxox/src/main/java/edu/ping/damian/cotxox/develop/domain/carrera/Carrera.java
@@ -3,7 +3,7 @@ package edu.ping.damian.cotxox.develop.domain.carrera;
 public class Carrera {
 
     //propiedades
-    private String tarjetaCredito = "";
+    private final String tarjetaCredito;
     private String origen = "";
     private String destino = "";
     private double distancia = 0.0d;
@@ -15,13 +15,40 @@ public class Carrera {
     }
 
     //getters
+    public String getTarjetaCredito() {
+        return this.tarjetaCredito;
+    }
 
-    //setters
+    public String getOrigen(){
+        return this.origen;
+    }
+
+    public String getDestino(){
+        return this.destino;
+    }
+
+    public double getDistancia(){
+        return this.distancia;
+    }
+
+    public void getCosteEstimado(){
+        //aloha!
+    }
+    
+     //setters
     public void setOrigen(String origen){
         this.origen = origen;
     }
 
     public void setDestino(String destino){
         this.destino = destino;
+    }
+
+    public void setDistancia(double distancia){
+        this.distancia = distancia;
+    }
+
+    public void setTiempoEsperado(int tiempoEsperadoMinutos){
+        this.tiempoEsperadoMinutos = tiempoEsperadoMinutos;
     }
 }
